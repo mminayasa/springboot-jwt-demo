a1c5350124dde768e0e94b3236e641f636068df7
Mistra Minayasa <mminayasa@gmail.com>
2023-05-27 17:04:36 +0700

    first init

---
+++ src/main/java/com/minayasa/jwtdemo27/JwtDemo27Application.java
@@ -0,0 +1,13 @@
+package com.minayasa.jwtdemo27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemo27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDemo27Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/minayasa/jwtdemo27/JwtDemo27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minayasa.jwtdemo27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDemo27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
